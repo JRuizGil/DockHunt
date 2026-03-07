d6fe2842da57458a639d44dfd95fa812adcb4963
Aritz <aritz_eguia@euneiz.com>
2026-03-07 23:16:52 +0100

    NPCs from side to side

---
+++ Assets/Scripts/NPCmovement.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCmovement : MonoBehaviour
+{
+    public List<Transform> wayPoint;
+    NavMeshAgent agent;
+
+    public int CurrentWayPointIndex = 0;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        Walking();
+    }
+    void Walking()
+    {
+        if (wayPoint.Count == 0)
+        {
+            return;
+        }
+        float distancetowaypoint = Vector3.Distance(wayPoint[CurrentWayPointIndex].position, transform.position);
+        if(distancetowaypoint <= 3)
+        {
+            CurrentWayPointIndex = (CurrentWayPointIndex + 1) % wayPoint.Count;
+        }
+
+        agent.SetDestination(wayPoint[CurrentWayPointIndex].position);
+
+    }
+}

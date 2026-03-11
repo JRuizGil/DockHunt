06658e540f455e454075c4c457ec8baf0b95b2f0
Aritz <aritz_eguia@euneiz.com>
2026-03-11 21:47:19 +0100

    Barco que se mueve y texto

---
+++ Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    private Vector3 StartingPos;
+    private Vector3 EndPos;
+    public float distance = 30f;
+    private float MoveTime = 10f;
+    private float CurrentMoveTime = 0;
+    void Start()
+    {
+        StartingPos = transform.position;
+        EndPos = transform.position + Vector3.forward * distance;
+    }
+
+    void Update()
+    {
+        CurrentMoveTime += Time.deltaTime;
+        float t = CurrentMoveTime / MoveTime;
+        t = Mathf.SmoothStep(0f, 1f, t);
+        transform.position = Vector3.Lerp(StartingPos, EndPos, t);
+    }
+}
